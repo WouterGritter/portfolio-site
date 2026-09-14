7f5dc96e13beffe9afad82339b28fbd083d628ff
Wouter Gritter <wouter@gritter.nl>
2026-09-14 21:32:50 +0200

    Add AI disclaimer

--- www/home.php
+++ www/home.php
@@ -79,9 +79,14 @@ $posts = array_slice($posts, 0, 4);
 <?php } ?>
 </ul>
 
+<p style="font-size:0.75em; padding-top:2em;">
+    Disclaimer: I use AI to help write my posts. Building these projects is what I love, but I also want to share them with the
+    world, since I believe they can bring real value to the right person. Writing isn't quite my thing though, so tools like
+    LLMs helps me get that story across.
+</p>
+
 <?php
 
 template_body_end();
 
 ?>
-
